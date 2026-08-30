155a6d5f56b7fa34b3a854fdbfbb821c17d3083e
Ostap Haivanovych <97313368+9uyone@users.noreply.github.com>
2026-08-31 00:41:16 +0300

    Add global exception handler

--- Middleware/GlobalExceptionHandler.cs
+++ Middleware/GlobalExceptionHandler.cs
@@ -1,4 +1,27 @@
-﻿namespace ABP_test_task.Middleware;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
-public class GlobalExceptionHandler {
+namespace ABP_test_task.Middleware;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler {
+	public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken) {
+		logger.LogError(exception, "Error occured: {Message}", exception.Message);
+
+		var (statusCode, title) = exception switch {
+			BadHttpRequestException => (StatusCodes.Status400BadRequest, "Bad request to API"),
+			UnauthorizedAccessException => (StatusCodes.Status401Unauthorized, "Unauthorized access to API"),
+			_ => (StatusCodes.Status500InternalServerError, "Internal server error")
+		};
+
+		var problemDetails = new ProblemDetails {
+			Status = statusCode,
+			Title = title,
+			Detail = exception.Message
+		};
+
+		httpContext.Response.StatusCode = problemDetails.Status.Value;
+		await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+		return true;
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using ABP_test_task.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi()
+	.AddProblemDetails()
+	.AddExceptionHandler<GlobalExceptionHandler>();
 
 var app = builder.Build();
 
@@ -11,8 +13,15 @@ if (app.Environment.IsDevelopment()) {
 	app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+app.UseExceptionHandler();
 
-
+app.MapGet("/{id}", (int id) => {
+	return id switch {
+		< 0 => throw new BadHttpRequestException("ID cannot be negative"),
+		> 100 => throw new UnauthorizedAccessException("ID cannot be greater than 100"),
+		_ => Results.Ok(new { Id = id, Message = "Request successful" })
+	};
+});
 
 app.Run();
